615e6073db93a5be8d8b3af11449f1ff09a0e3b7
keklon <kekw@tuta.com>
2025-03-14 11:22:36 +0100

    Up yazi

--- yazi/init.lua
+++ yazi/init.lua
@@ -30,14 +30,14 @@ Root.layout = function(self, ...)
   end
 end
 
-local function setup()
-	local old_layout = Tab.layout
-
-	Status.redraw = function() return {} end
-	Tab.layout = function(self, ...)
-		self._area = ui.Rect { x = self._area.x, y = self._area.y, w = self._area.w, h = self._area.h + 1 }
-		return old_layout(self, ...)
-	end
-end
-
-setup()
+-- local function setup()
+-- 	local old_layout = Tab.layout
+--
+-- 	Status.redraw = function() return {} end
+-- 	Tab.layout = function(self, ...)
+-- 		self._area = ui.Rect { x = self._area.x, y = self._area.y, w = self._area.w, h = self._area.h + 1 }
+-- 		return old_layout(self, ...)
+-- 	end
+-- end
+--
+-- setup()
